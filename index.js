30837cce098836b80e462823fadf6ecb32e93b6b
Rhiannon Shoults <rhiannon.csw@gmail.com>
2018-05-21 12:03:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 function findMatching(drivers, findName) {
-  drivers.filter(function (name) {return name.toLowerCase() === findName.toLowerCase();});
+  return drivers.filter(function (name) {return name.toLowerCase() === findName.toLowerCase();});
 }
